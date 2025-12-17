be256a11936cd6740a80f4620cca34fdf4b0d681
marwa <242452810+marwamusal@users.noreply.github.com>
2025-12-17 23:44:23 +0000

    Refactor

--- Gene_Assembling/11_latest_sy14pilon.sh
+++ Gene_Assembling/11_latest_sy14pilon.sh
@@ -7,37 +7,45 @@
 #SBATCH --time=12:00:00
 #SBATCH --partition=cpu
 
-# -------------------------
 # 1. Purge modules
-# -------------------------
 module purge
 
-# -------------------------
-# 2. Go to assembly directory
-# -------------------------
+
+# 2. Load anaconda and activate pilon environment
+module load anaconda3/2022.10-gcc-13.2.0
+source activate /scratch/grp/msc_appbio/Group2_ABCC/conda_envs/pilon_env
+
+
+# 3. Set Java heap memory 
+export _JAVA_OPTIONS="-Xmx55G"
+
+
+# 4. Go to assembly directory
 cd /scratch/grp/msc_appbio/Group2_ABCC/Gene_Assembling/assembly/SY14
 
-# -------------------------
-# 3. Add Pilon environment to PATH
-#    (so Pilon and Java can be found)
-# -------------------------
-export PATH=/scratch/grp/msc_appbio/Group2_ABCC/conda_envs/pilon_env/bin:$PATH
 
-# -------------------------
-# 4. Optional: remove old Pilon logs to avoid clutter
-# -------------------------
+# 5. Remove old Pilon logs to avoid clutter
 rm -f SY14_pilon*  # remove old outputs if any
 
-# -------------------------
-# 5. Run Pilon
-# -------------------------
+
+# 6. Run Pilon 
 pilon \
   --genome SY14.contigs.fasta \
-  --frags SY14.contigs.bam \
+  --frags SY14_illumina_mapped.bam \
   --output SY14_pilon \
-  --threads 4
- --Xmx55G
-# -------------------------
-# Done!
-# -------------------------
+  --changes
+
+
 echo "Pilon polishing completed at $(date)"
+
+
+# Verification
+
+echo "Number of contigs in polished assembly:"
+grep -c ">" SY14_pilon.fasta
+
+echo "Assembly size:"
+grep -v ">" SY14_pilon.fasta | tr -d '\n' | wc -c
+
+echo "Number of corrections made:"
+wc -l SY14_pilon.changes
